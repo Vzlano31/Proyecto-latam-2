a430983720bc0273b258448235545f0ba53cdf21
vzlano31 <luish3007@gmail.com>
2023-07-03 13:38:29 -0430

    cambios en javascript

--- js/main.js
+++ js/main.js
@@ -4,22 +4,10 @@ console.log(menuItems);
 
 menuItems.forEach(function (item) {
     item.addEventListener("click", function(e) {
-        const currenItem = documen.querySelector(".active");
+        const currentItem = document.querySelector(".active");
         currentItem.classList.remove("active");
         e.target.classList.add("active");
     });
 });
-
-/* const body = document.querySelector("body");
-const iconos= document.querySelectorAll(".icon");
-
-const boton = document.querySelector("#btn-dark");
-
-boton.addEventListener("clic",fuction (e) {
-    body.classList.toggle ("modo_oscuro");
-    iconos.forEach(fuction (icono){
-        icono.classList.toggle("color_icon");
-    });
-}); */
 
 
